461f059ba9363a0d074c75618422bbaa3d4f8197
zhouyi <zhouyi@ibuka.cn>
2016-01-15 18:27:20 +0800

    add code

--- Knowledge/MachineLearning/ML-AndrewNg/ProgrammingExercisesInPython/week 3/ex2.py
+++ Knowledge/MachineLearning/ML-AndrewNg/ProgrammingExercisesInPython/week 3/ex2.py
@@ -1,15 +1,37 @@
 # !/usr/bin/env python
 # encoding: utf-8
 
+from os.path import *
+
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+DATA_PATH = join(dirname(__file__), 'ex2data1.txt')
+
+
+def load_data():
+    data = np.loadtxt(DATA_PATH, delimiter=',')
+    X, y = data[:, :2], data[:, 2]
+    return X, y
+
+
+def plot_data(X, y):
+    plt.plot(X, y)
+    plt.xlabel('Exam 1 score')
+    plt.ylabel('Exam 2 score')
 
 
 def part_1():
-    pass
+    print ('Plotting data with + indicating (y = 1) examples and o indicating '
+           '(y = 0) examples')
+    X, y = load_data()
+    plot_data(X, y)
+    plt.show(block=True)
 
 
 def main():
-    pass
+    part_1()
 
 
 if __name__ == '__main__':
