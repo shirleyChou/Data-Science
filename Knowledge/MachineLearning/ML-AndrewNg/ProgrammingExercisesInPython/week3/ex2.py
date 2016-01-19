af877b9767dd023f49f007de3d3ac554169d9dba
zhouyi <zhouyi@ibuka.cn>
2016-01-19 18:34:41 +0800

    .

--- Knowledge/MachineLearning/ML-AndrewNg/ProgrammingExercisesInPython/week3/ex2.py
+++ Knowledge/MachineLearning/ML-AndrewNg/ProgrammingExercisesInPython/week3/ex2.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 from os.path import *
+from math import exp
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,11 +18,22 @@ def load_data():
 
 
 def plot_data(X, y):
-    plt.scatter(X[y == 1, 0], X[y == 1, 1], c='b', s=40, marker='+', linewidth=2, label="Admitted")
-    plt.scatter(X[y == 0, 0], X[y == 0, 1], c='y', s=40, marker='o', linewidth=1, label="Not admitted")
+    plt.scatter(X[y == 1, 0], X[y == 1, 1], c='b', s=40, marker='+',
+                linewidth=2, label="Admitted")
+    plt.scatter(X[y == 0, 0], X[y == 0, 1], c='y', s=40, marker='o',
+                linewidth=1, label="Not admitted")
     plt.xlabel('Exam 1 score')
     plt.ylabel('Exam 2 score')
     plt.legend()
+
+
+def sigmoid_func(z):
+    g = 1.0 / (1 + exp(-z))
+    return g
+
+
+def cost_function():
+    pass
 
 
 def part_1():
@@ -32,8 +44,16 @@ def part_1():
     plt.show(block=True)
 
 
+def part_2():
+    X, y = load_data()
+    m, n = X.shape
+    
+
+
+
 def main():
-    part_1()
+    # part_1()
+    part_2()
 
 
 if __name__ == '__main__':
