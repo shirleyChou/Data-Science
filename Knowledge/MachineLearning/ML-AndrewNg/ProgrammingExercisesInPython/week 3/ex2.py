0c557f629401ec1dfbe2e84c33f777257ca4e2bd
ZhouYi <shirleychou1@gmail.com>
2016-01-19 08:20:36 +0800

    to LR

--- Knowledge/MachineLearning/ML-AndrewNg/ProgrammingExercisesInPython/week 3/ex2.py
+++ Knowledge/MachineLearning/ML-AndrewNg/ProgrammingExercisesInPython/week 3/ex2.py
@@ -17,9 +17,11 @@ def load_data():
 
 
 def plot_data(X, y):
-    plt.plot(X, y)
+    plt.scatter(X[y == 1, 0], X[y == 1, 1], c='b', s=40, marker='+', linewidth=2, label="Admitted")
+    plt.scatter(X[y == 0, 0], X[y == 0, 1], c='y', s=40, marker='o', linewidth=1, label="Not admitted")
     plt.xlabel('Exam 1 score')
     plt.ylabel('Exam 2 score')
+    plt.legend()
 
 
 def part_1():
